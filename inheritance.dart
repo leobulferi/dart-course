7cb52d6a5bc99b68d7a5e80530e66f76c1004b02
leobulferi <bulferileonardo@gmail.com>
2024-03-18 13:24:33 +0100

    course

--- inheritance.dart
+++ inheritance.dart
@@ -12,11 +12,14 @@ void main() {
   //We can also have
   Vehicle car = Car(); // you can access all the properties of a vehicle but not all the properties for the car. So I have to do like this
   print((car as Car).noOfWheels);
+
+  car.accelerate();
+  print(speed); //return 25
 }
 class SomeClass {
   int speed = 15;
 
-  void accelarate() {
+  void accelerate() {
     speed += 30;
   }
 }
@@ -25,6 +28,7 @@ class Vehicle extends SomeClass{
   int speed = 10;
   bool isEngineWorking = false;
 
+  @override //if we put this is gonna consider this one before the one above
   void accelerate() {
     speed += 10;
   }
@@ -47,6 +51,69 @@ class Truck extends Vehicle { //whenever we use extends we are extending the fun
 }
 
 
+
+
+
+
+
+
+// Implements and abstract classes
+void main() {
+  final car = Car();
+  print(car.noOfWheels);
+
+}
+
+class OtherClass {
+  bool isLightOn = true;
+  bool isEngineWorking = false;
+  int noOfWheels = 10;
+}
+
+class Vehicle{
+  void accelerate() {
+    print('accelerating vehicle ahah');
+  }
+
+}
+
+class Car extends OtherClass implements Vehicle {  //I have access to OtherClass but I have to implement Vehicle
+  @override
+  bool isEngineWorking = true;
+
+  @override
+  bool isLightOn = true;
+
+  @override
+  int noOfWheels = 4;
+
+  @override
+  void accelerate() {
+    super.accelerate();   //it refer to istance classes of the vehicle. U can use it just with extends. You now modify the contents of the parent class in the subclass
+    super.isEngineWorking = true;  
+  }
+
+}
+
+class Truck implements Vehicle { 
+   @override
+  bool isEngineWorking = true;
+
+  @override
+  bool isLightOn = true;
+
+  @overrideint noOfWheels = 6;
+
+  @override
+  void accelerate() {
+    print('accelerating the truck');
+  }
+
+}
+
+class Bike implements Vehicle {
+
+}
 
 
 //Object Oriented Programming(OOP)
